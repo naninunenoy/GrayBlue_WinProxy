c11b3df95644804975c0f683c63c8325da2b873b
nakano yosuke <naninuneno0411@gmail.com>
2019-04-14 16:32:09 +0900

    fix interface

---
+++ GrayBlue_WinProxy/API/IClientRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagicOnion;
+
+namespace GrayBlue_WinProxy.API {
+    interface IClientRequest : IStreamingHub<IClientRequest, IServerNotify> {
+        Task<bool> CheckBLEAvailableAsync();
+        Task<string[]> ScanAsync();
+        Task<bool> ConnectAsync(string deviceId);
+        void Disconnect(string deviceId);
+    }
+}
---
+++ GrayBlue_WinProxy/API/IServerNotify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrayBlue_WinProxy.API {
+    interface IServerNotify {
+        void OnSensorUpdateNotify(string deviceId, float[] acc, float[] gyro, float[] mag, float[] quat);
+        void OnButtonUpdateNotify(string deviceId, bool isPressed, string button, float time);
+        void OnDeviceLost(string deviceId);
+    }
+}
