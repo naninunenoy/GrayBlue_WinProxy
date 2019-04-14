985008246642df71ac5e275b325e5a38a0d1b63a
nakano yosuke <naninuneno0411@gmail.com>
2019-04-14 15:26:14 +0900

    wake up MagicOnion

--- GrayBlue_WinProxy/MainPage.xaml.cs
+++ GrayBlue_WinProxy/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Grpc.Core;
+using MagicOnion.Server;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -20,11 +22,28 @@ namespace GrayBlue_WinProxy {
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
-    public sealed partial class MainPage : Page {
+    public sealed partial class MainPage : Page, IDisposable {
+        Server server;
+        string serverHostName = "localhost";
+        int serverPortNo = 12345;
+
         public MainPage() {
             this.InitializeComponent();
+            // set windows size
             ApplicationView.PreferredLaunchViewSize = new Size { Height = 320, Width = 369 };
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            // server set up
+            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+            server = new Server {
+                Services = { MagicOnionEngine.BuildServerServiceDefinition(isReturnExceptionStackTraceInErrorDetail: true) },
+                Ports = { new ServerPort(serverHostName, serverPortNo, ServerCredentials.Insecure) }
+            };
+            // server start
+            server.Start();
+        }
+
+        public void Dispose() {
+            server?.KillAsync();
         }
     }
 }
