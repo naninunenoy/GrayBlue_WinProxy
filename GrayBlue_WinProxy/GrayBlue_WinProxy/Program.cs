f7b81db851d8b62c754f379bf94806ae0669c058
nakano yosuke <naninuneno0411@gmail.com>
2019-05-02 19:21:13 +0900

    add GrayBlueUWPCore.dll

--- GrayBlue_WinProxy/GrayBlue_WinProxy/Program.cs
+++ GrayBlue_WinProxy/GrayBlue_WinProxy/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace GrayBlue_WinProxy {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            GrayBlueUWPCore.Plugin.Instance.Scan().ContinueWith(x => {
+                var lst = x.Result;
+                Console.WriteLine(string.Join(",", lst));
+            });
+            Console.ReadKey();
         }
     }
 }
