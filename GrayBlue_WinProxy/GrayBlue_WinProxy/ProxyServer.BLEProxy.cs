b9a145f1eaa00c7d362c28d662b4f144b8c8c864
nakano yosuke <naninuneno0411@gmail.com>
2019-05-24 01:05:44 +0900

    thread safe

--- GrayBlue_WinProxy/GrayBlue_WinProxy/ProxyServer.BLEProxy.cs
+++ GrayBlue_WinProxy/GrayBlue_WinProxy/ProxyServer.BLEProxy.cs
@@ -12,35 +12,48 @@ using GrayBlue_WinProxy.GrayBlue;
 namespace GrayBlue_WinProxy {
     partial class ProxyServer : IBLENotify {
         private readonly RequestAgent requestAgent;
+        object lockObject = new object();
 
         void IBLENotify.OnRequestDone(string requestName, string requestParam, string response) {
             var json = JsonConverter.ToMethodResultJson(requestName, requestParam, response);
-            RunTaskOn(context, Broadcast(json));
+            lock (lockObject) {
+                RunTaskOn(context, Broadcast(json));
+            }
         }
 
         void IBLENotify.OnConnectSuccess(string deviceId) {
             var json = JsonConverter.ToConnectResultJson(deviceId, true);
-            RunTaskOn(context, Broadcast(json));
+            lock (lockObject) {
+                RunTaskOn(context, Broadcast(json));
+            }
         }
 
         void IBLENotify.OnConnectFail(string deviceId) {
             var json = JsonConverter.ToConnectResultJson(deviceId, false);
-            RunTaskOn(context, Broadcast(json));
+            lock (lockObject) {
+                RunTaskOn(context, Broadcast(json));
+            }
         }
 
         void IBLENotify.OnDeviceLost(string deviceId) {
             var json = JsonConverter.ToDeviceLostJson(deviceId);
-            RunTaskOn(context, Broadcast(json));
+            lock (lockObject) {
+                RunTaskOn(context, Broadcast(json));
+            }
         }
 
         void IBLENotify.OnButtonOperation(string devceId, bool isPush, string button, float time) {
             var json = JsonConverter.ToButtonNotifyJson(devceId, isPush, button, time);
-            RunTaskOn(context, Broadcast(json));
+            lock (lockObject) {
+                RunTaskOn(context, Broadcast(json));
+            }
         }
 
         void IBLENotify.OnIMUDataUpdate(string deviceId, float[] acc, float[] gyro, float[] mag, float[] quat) {
             var json = JsonConverter.ToIMUNotifyJson(deviceId, acc, gyro, mag, quat);
-            RunTaskOn(context, Broadcast(json));
+            lock (lockObject) {
+                RunTaskOn(context, Broadcast(json));
+            }
         }
 
         void RunTaskOn(SynchronizationContext on, Task task) {
